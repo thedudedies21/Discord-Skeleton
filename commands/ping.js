e4c147def8afc4f11da5de2ed6ac47fcb8369f52
thedudedies21 <80545175+thedudedies21@users.noreply.github.com>
2024-10-01 13:16:54 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,9 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+	        .setContexts([0,1,2])
+		.setIntegrationTypes([0]),
 	async execute(interaction) {
 			interaction.reply({content:'Pong!'});	
 	},
